e70a247be8012e360ef8495f6db8903b05277478
Lilia-Sirgalina <lilia_developer@hotmail.com>
2025-02-12 19:03:46 +0100

    first commit

---
+++ waves/waves.js
@@ -0,0 +1,52 @@
+const button = document.querySelector('#myButton');
+
+document.querySelector('#player').pause();
+
+button.addEventListener('click', startTimer);
+
+const timer = 1;
+let amountTime = timer * 60;
+let timerID = null;
+
+function startTimer() {    
+    if (timerID === null) {
+    timerID = setInterval(calculateTime, 1000);
+}
+
+    document.querySelector('#player').play();
+    document.querySelector('#myVideo').play();
+}
+
+function calculateTime() {
+    
+    const countDown = document.querySelector('#countdown');
+    
+    let minutes = Math.floor(amountTime/60);
+    let seconds = amountTime%60;
+    
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    countDown.textContent = `${minutes} : ${seconds}`;
+    amountTime--;        
+
+    if (amountTime < 0) {        
+        stopTimer();
+        countDown.textContent = "Relax...";
+        document.querySelector('#player').pause();
+        amountTime = timer*60;               
+    }       
+}
+
+function stopTimer() {
+    clearInterval(timerID);
+    timerID = null;
+}
+
+
+
+
+
+
+
